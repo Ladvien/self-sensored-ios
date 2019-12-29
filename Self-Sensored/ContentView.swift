c4e6289233cd75a0897b9bb16bf16afc54775ebb
ladvien <honeysucklehardware@gmail.com>
2019-12-29 06:18:30 -0600

    Initial Commit

---
+++ Self-Sensored/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Self-Sensored
+//
+//  Created by Casey Brittain on 12/29/19.
+//  Copyright © 2019 Casey Brittain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
